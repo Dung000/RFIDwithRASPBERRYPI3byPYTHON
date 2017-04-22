f0f419b9041d4ec9bd070fe8981e50f67f455dce
Dung000 <dungKMT.spkt@gmail.com>
2017-04-22 18:11:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup (
     name = 'SPI-Py',
     author='Louis Thiery',
     url='https://github.com/lthiery/SPI-Py',
-    download_url='https://github.com/lthiery/SPI-Py/archive/master.zip',
+    download_url='https://github.com/Dung000/RFIDwithRASPBERRYPI3byPYTHON/archive/master.zip',
     version = '1.0',
     description = 'SPI-Py: Hardware SPI as a C Extension for Python',
     license='GPL-v2',
